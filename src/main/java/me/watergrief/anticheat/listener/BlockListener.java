69733e0f65cb56c52d8271a027d0c7acda13552b
arbitraf3 <arbitraxstart@gmail.com>
2026-02-24 22:28:25 +0200

    Delete src directory

--- src/main/java/me/watergrief/anticheat/listener/BlockListener.java
+++
@@ -1,8 +0,0 @@
-
-package me.watergrief.anticheat.listener;
-
-import org.bukkit.event.Listener;
-
-public class BlockListener implements Listener {
-    // Scaffold / FastPlace can be implemented here
-}
